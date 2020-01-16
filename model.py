89c9422d2f183c1a5d199288b98fe8dde2bc4b2f
Nguyễn Phú Hiển <15957014+nguyenphuhien13@users.noreply.github.com>
2020-01-16 14:01:35 +0100

    Update model.py

--- model.py
+++ model.py
@@ -8,6 +8,72 @@ from train_and_predict import *
 
 IMAGE_ORDERING = 'channels_last'
 
+def SegNet1(nClasses, input_height=256, input_width=256):
+    img_input = Input(shape=(input_height, input_width, 3))
+    kernel_size = 3
+
+    # encoder
+    for n_filter in [64, 128, 256, 512, 512]:
+        if n_filter == 64:
+            # x = ZeroPadding2D((1, 1))(img_input)
+            x = Conv2D(n_filter, (kernel_size, kernel_size), padding='same',
+                  kernel_initializer=orthogonal())(img_input)
+        else:
+            x = Conv2D(n_filter, (kernel_size, kernel_size), padding='same',
+                  kernel_initializer=orthogonal())(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = MaxPooling2D(pool_size=(2, 2))(x)
+    # original -> 128x128 -> 64x64 -> 32x32 -> 16x16 -> 8x8
+
+    # decoder
+    for n_filter in [512, 512, 256, 128, 64]:
+        x = UpSampling2D(size=(2, 2))(x)
+        x = Conv2D(n_filter, (kernel_size, kernel_size), padding='same',
+                  kernel_initializer=orthogonal())(x)
+        x = BatchNormalization()(x)
+
+    ## penultimate
+    x = Conv2D(nClasses, (1, 1), padding='valid',
+               kernel_initializer=he_normal(), kernel_regularizer=l2(0.005))(x)
+    x = Reshape((nClasses, input_height * input_width))(x)
+    x = Permute((2, 1))(x)
+
+    x = Activation('softmax')(x)
+    model = Model(img_input, x, name='SegNet')
+    return model
+
+def SegNet2(nClasses, input_height=256, input_width=256):
+    img_input = Input(shape=(input_height, input_width, 3))
+    x = img_input
+    
+    kernel = 3
+    pad = 1
+    pool_size = 2
+
+    # encoder
+    for n_filter in [64, 128, 256, 256, 512]:
+        x = ZeroPadding2D((pad, pad))(x)
+        x = Conv2D(n_filter, (kernel, kernel),padding='valid')(x)
+        x = BatchNormalization()(x)
+        x = Activation('relu')(x)
+        x = MaxPooling2D((pool_size, pool_size))(x)
+
+    # decoder
+    for n_filter in [512, 256, 256, 128, 64]:
+        x = UpSampling2D((pool_size, pool_size))(x)
+        x = ZeroPadding2D((pad, pad))(x)
+        x = Conv2D(n_filter, (kernel, kernel), padding='valid')(x)
+        x = BatchNormalization()(x)
+
+    x = Conv2D(n_classes, (3, 3), padding='same')(x)
+    output_height, output_width = x.shape[1], x.shape[2]
+    x = Reshape((output_height*output_width, -1))(x)
+    x = Activation('softmax')(x)
+
+    model = Model(img_input, x, name='SegNet')
+    return model
+
 def get_segmentation_model(input, output):
 
     img_input = input
